909094f024a1c2a3fa0f094302104537c1e6699a
Alex Close <ajclose@gmail.com>
2017-07-13 19:27:17 -0500

    Add registration page

--- routes/registration.js
+++ routes/registration.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const User = require("../models/User")
 
 router.get("/register", function(req, res) {
   res.render("registration")
+})
+
+router.post('/register', function(req, res) {
+  User.findOne({
+    username: req.body.username
+  })
+  .then(function(user) {
+    if (user) {
+      res.render("registration", {
+        userError: "That username already exists"
+      })
+    } else {
+      const user = new User()
+      user.username = req.body.username
+      user.email = req.body.email
+      user.password = req.body.password
+      user.save()
+      .then(function(user) {
+        res.json(user)
+      })
+    }
+  })
 })
 
 module.exports = router

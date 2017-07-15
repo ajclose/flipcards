ab523391e25eef81601b79179056f7260bcbd74b
Alex Close <ajclose@gmail.com>
2017-07-15 16:08:12 -0500

    Display topics and questions

--- public/js/client.js
+++ public/js/client.js
@@ -1,22 +1,58 @@
 function fetchDecks() {
-  document.querySelector('.decks').textContent = ''
+  deckContainer.textContent = ''
   fetch('/api/decks', {
     credentials: 'include'
   })
   .then(function(res) {
-    return res.json
+    return res.json()
   })
   .then(function(json) {
     for (var i = 0; i < json.length; i++) {
       const deck = json[i]
       const html = `
+      <div class="deck" id="${deck._id}">
       <h3>${deck.topic}</h3>
+      </div>
       `
 
-      document.querySelector('.decks').insertAdjacentHTML('beforeend', html)
+      deckContainer.insertAdjacentHTML('beforeend', html)
     }
+
+    const decks = document.querySelectorAll(".deck")
+    for (var i = 0; i < decks.length; i++) {
+      const deck = decks[i]
+      deck.addEventListener("click", function(event) {
+        const deckId = deck.id
+        fetchDeck(deckId)
+      })
+      }
   })
 }
+
+
+function fetchDeck(id) {
+  deckContainer.textContent = ''
+  fetch(`/api/decks/${id}`, {
+    credentials: 'include'
+})
+.then(function(res) {
+  return res.json()
+})
+.then(function(json) {
+  for (var i = 0; i < json.cards.length; i++) {
+    const card = json.cards[i]
+
+    const html = `
+    <div class="card" id="${card._id}">
+    <h3>${card.question}</h3>
+    </div>
+    `
+
+    deckContainer.insertAdjacentHTML('beforeend', html)
+  }
+})
+}
+
 
 const deckContainer = document.querySelector('.decks')
 if (deckContainer) {

13e8314832f71cbfa11429c21b99a9eb7d0191aa
Alex Close <ajclose@gmail.com>
2017-07-18 19:58:35 -0500

    Add forms for adding topic and question

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,36 @@
+function createDeck(deck) {
+  return  `
+  <div class="deck" id="${deck._id}">
+  <h4>${deck.topic}</h4>
+  </div>
+  `
+}
+
+function createCard(card) {
+  return `
+  <div class="card" id="${card._id}">
+  <h3>${card.question}</h3>
+  </div>
+  <div class="answer">
+  Show answer
+  </div>
+  `
+}
+
 function fetchDecks() {
   deckContainer.textContent = ''
+
+  const topicForm = `
+  <form class="addTopic" action="/decks" method="post">
+  <fieldset>
+  <legend>Add Topic</legend>
+  <label for="Topic">Topic</label>
+  <input type="text" id="topic" name="topic" value="" placeholder="Enter Topic">
+  <input type="submit" name="submit" value="Add Topic">
+  </fieldset>
+  </form>
+  `
+  deckContainer.insertAdjacentHTML('afterbegin', topicForm)
   fetch('/api/decks', {
     credentials: 'include'
   })
@@ -7,17 +38,15 @@ function fetchDecks() {
     return res.json()
   })
   .then(function(json) {
+
+    const title = '<h3>Select a topic:</h3>'
+    deckContainer.insertAdjacentHTML('beforeend', title)
+
     for (var i = 0; i < json.length; i++) {
       const deck = json[i]
-      const html = `
-      <div class="deck" id="${deck._id}">
-      <h3>${deck.topic}</h3>
-      </div>
-      `
-
+      const html = createDeck(deck)
       deckContainer.insertAdjacentHTML('beforeend', html)
     }
-
     const decks = document.querySelectorAll(".deck")
     for (var i = 0; i < decks.length; i++) {
       const deck = decks[i]
@@ -26,12 +55,49 @@ function fetchDecks() {
         fetchDeck(deckId)
       })
       }
+      const topicForm = document.querySelector("form.addTopic")
+      if (topicForm) {
+          topicForm.addEventListener("submit", function(event) {
+            event.preventDefault()
+            formData = {
+              topic: document.querySelector("#topic").value
+            }
+
+            fetch("/api/decks", {
+              method: "POST",
+              credentials: "include",
+              body: JSON.stringify(formData),
+              headers: {
+                "content-type": "application/json"
+              }
+            })
+            .then(function(res) {
+              return res.json()
+            })
+            .then(function(json) {
+              fetchDecks()
+            })
+          })
+      }
   })
 }
 
 
 function fetchDeck(id) {
+  const deckid = id
   deckContainer.textContent = ''
+  const cardForm =  `
+    <form class="addCard" action="/api/decks/${id} method="post">
+    <fieldset>
+    <legend>Add question</legend>
+    <label for="question">Question:</label>
+    <input type="text" id="question" name="question" value="" placeholder="Enter Question">
+    <label for="answer">Answer:</label>
+    <input type="text" id="answer" name="answer" value="" placeholder="Enter Answer">
+    <input type="submit" name="submit" value="Add Card">
+    </fieldset>
+    `
+    deckContainer.insertAdjacentHTML('afterbegin', cardForm)
   fetch(`/api/decks/${id}`, {
     credentials: 'include'
 })
@@ -41,18 +107,44 @@ function fetchDeck(id) {
 .then(function(json) {
   for (var i = 0; i < json.cards.length; i++) {
     const card = json.cards[i]
-
-    const html = `
-    <div class="card" id="${card._id}">
-    <h3>${card.question}</h3>
-    </div>
-    `
-
+    const html = createCard(card)
     deckContainer.insertAdjacentHTML('beforeend', html)
   }
-})
-}
+  const answers = document.querySelectorAll('.answer')
+    for (var j = 0; j < answers.length; j++) {
+      answer = answers[j]
+      const answerText = json.cards[j].answer
+      answer.addEventListener("click", function(event) {
+        event.target.innerHTML = answerText
+      })
+    }
+    const cardForm = document.querySelector("form.addCard")
+    if (cardForm) {
+        cardForm.addEventListener("submit", function(event) {
+          event.preventDefault()
+          formData = {
+            question: document.querySelector("#question").value,
+            answer: document.querySelector("#answer").value
+          }
 
+          fetch(`/api/decks/${deckid}`, {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(formData),
+            headers: {
+              "content-type": "application/json"
+            }
+          })
+          .then(function(res) {
+            return res.json()
+          })
+          .then(function(json) {
+            fetchDeck(deckid)
+          })
+        })
+      }
+  })
+}
 
 const deckContainer = document.querySelector('.decks')
 if (deckContainer) {
